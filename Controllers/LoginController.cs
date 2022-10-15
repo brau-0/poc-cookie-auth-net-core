7b39f38b74a37cd81e1682a31c3ed2fa5e34839b
Braulio Guzman <braulio@c2go.com.mx>
2022-10-14 22:02:07 -0500

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using POCCookieAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace POCCookieAuth.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> PerformLogin([Bind] User userdetails)
+        {
+            if ((!string.IsNullOrEmpty(userdetails.UserId)) && (!string.IsNullOrEmpty(userdetails.Password)))
+            {
+                if ((userdetails.UserId.Equals("admin") && userdetails.Password.Equals("admin")))
+                {
+                    var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userdetails.UserId),
+                    new Claim(ClaimTypes.Role, "User"),
+                };
+                    var claimsIdentity = new ClaimsIdentity(
+                        claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var authProperties = new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.Now.AddMinutes(10),
+                    };
+                    await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ CustomCookieAuthenticationEvents.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POCCookieAuth
+{
+    public class CustomCookieAuthenticationEvents : CookieAuthenticationEvents
+    {
+        public override async Task ValidatePrincipal(CookieValidatePrincipalContext context)
+        {
+            ///Check against the database if user object has changed since cookie issued DateTime,
+            ///if yes then SignOut (Invalidate) the cookie
+            //var userPrincipal = context.Principal;
+
+            //// Look for the LastChanged claim.
+            //var lastChanged = (from c in userPrincipal.Claims
+            //                   where c.Type == "LastChanged"
+            //                   select c.Value).FirstOrDefault();
+
+            //if (string.IsNullOrEmpty(lastChanged) ||
+            //    !_userRepository.ValidateLastChanged(lastChanged))
+            //{
+            //    context.RejectPrincipal();
+
+            //    await context.HttpContext.SignOutAsync(
+            //        CookieAuthenticationDefaults.AuthenticationScheme);
+            //}
+            if (false)//if some change in user access
+            {
+                context.RejectPrincipal();
+                await context.HttpContext.SignOutAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+            
+        }
+    }
+}
